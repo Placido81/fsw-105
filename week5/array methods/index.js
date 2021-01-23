bbfa7fbf097235d469d3e93292c3638597939a7a
Placido81 <mark.olivo@bryanuniversity.edu>
2021-01-23 11:04:07 -0500

    practice

---
+++ week5/array methods/index.js
@@ -0,0 +1,16 @@
+
+var fruit = [ "banana", "apple", "orange", "watermelon"]
+
+var vegetables = [ "carrots", "tomatos", "pepper", "lettuce"]
+function myArrays(){
+// Remove last item fr. Array:
+    vegetables.pop();
+    console.log("Vegetables:", vegetables);
+    
+
+    
+    // Remove first item:
+    fruit.shift();
+    console.log("Fruit:",fruit);
+}
+//console.log(arrayExercise);
